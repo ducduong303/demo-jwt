8207b08ed1a92e7a0c2879ef6e0be4c0a1c2c309
ducduong303 <57469702+ducduong303@users.noreply.github.com>
2021-08-23 15:51:19 +0700

    commit

--- client/src/api/http.jsx
+++ client/src/api/http.jsx
@@ -24,21 +24,6 @@ http.interceptors.response.use(function (response) {
     return response;
 }, function (error) {
     if (error.response && error.response.data) {
-        // if (error.response.data.code === 401 && window.location.pathname !== '/login') {
-        //     try {
-        //         clearTimeout(timeoutTokenExpired)
-        //         timeoutTokenExpired = setTimeout(() => {
-        //             store.dispatch(clearToken());
-        //             Promise.reject(error);
-        //             localStorage.removeItem(EVN_TOKEN)
-        //             sessionStorage.removeItem(EVN_TOKEN)
-        //             history.push("/login");
-        //             NotificationError(ERROR, TOKEN_EXPIRED)
-        //         }, 300)
-        //     } catch (ex) {
-        //         console.log("interceptor -> ex", ex)
-        //     }
-        // }
         return Promise.reject(error.response.data);
     } else {
         return Promise.reject(error);
